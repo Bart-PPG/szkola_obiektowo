2d5f4406fc3fd7383e2a72d6798cb611e09c5272
Bart-PPG <bartek.nitek@onet.pl>
2024-09-18 12:09:11 +0200

    pola prywatne

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Osoba osobaJas = new Osoba("Jaś","Śliweczka",7);
-
-        //niezgodne z regula hermetyzacji
-        System.out.println(osobaJas.imie);
-        System.out.println(osobaJas.nazwisko);
-        osobaJas.imie = "Janke";
-        System.out.println(osobaJas.imie);
-        //hermetyzacja
-        //powinno byc tak zeby program nie mogl zmieniac wlasnosci klas
-
+        //wywoluje konstruktor klasy osoba
+        System.out.println(osobaJas.getImie());
+        System.out.println(osobaJas.getNazwisko());
+        osobaJas.setImie("Jan");
+        System.out.println(osobaJas.getImie());
     }
 }
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Osoba {
-    String imie;
-    String nazwisko;
-    int wiek;
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+    //prywatne wartosci sa dostepne tylko w tej klasie
 
     //konstruktor to metoda wywoływana w momencie tworzenia obiektu
 
@@ -10,6 +13,41 @@ public class Osoba {
         //przypisz do wlasnosci klasy imie (this wskazuje ne ten )
         //imie ktore jest parametrem
         this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+    //metody dostepowe
+    //gettery i settery
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        //wpisywanie hasla z klawiatury
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj haslo do zmiany imienia");
+        String haslo = scanner.next();
+        //jesli haslo qwe123 to zmien w przciwnym wypadku komunikat
+        if(haslo.equals("qwe123")){
+            this.imie = imie;
+        }
+        else {
+            System.out.println("nie możemy zmienic imienia");
+        }
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
         this.wiek = wiek;
     }
 }
