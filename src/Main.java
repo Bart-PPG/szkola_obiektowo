56f20ac770fea03e6a146460bd45a6b7392da092
Bart-PPG <bartek.nitek@onet.pl>
2024-09-18 13:24:59 +0200

    ograniczenia dziedziczenia

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,9 @@ public class Main {
         Uczen uczen3 = new Uczen("Danielo","Stary");
         System.out.println("Liczba uczniow"+Uczen.liczbaObiektow);
         System.out.println(uczen3);
+
+            Nauczyciel nauczycielSlowik = new Nauczyciel("Marlena","Slowik", 30,"Matematyka");
+        System.out.println(nauczycielSlowik);
+
     }
 }
--- src/Nauczyciel.java
+++ src/Nauczyciel.java
@@ -1,2 +1,22 @@
-public class Nauczyciel {
+public sealed  class Nauczyciel
+        extends Osoba
+        permits Wychowawca{
+    //sealed ograniczenie dzieciczenia
+    //permits jaka klasa moze dziedziczyc
+    private String przedmiot;
+
+    public Nauczyciel(String imie, String nazwisko, int wiek, String przedmiot) {
+        super(imie, nazwisko, wiek);
+        this.przedmiot = przedmiot;
+    }
+
+    @Override
+    public String toString() {
+        return "Nauczyciel{" +
+                "imie: '" + getImie()+
+                "nazwisko: " + getNazwisko()+
+                "wiek: " + getWiek()+
+                "przedmiot='" + przedmiot +
+                '}';
+    }
 }
